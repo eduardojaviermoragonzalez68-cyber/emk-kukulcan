9684a8edac394f2008ad3beae8912703821ac234
Eduardo Javier Mora González <eduardojaviermoragonzalez68@gmail.com>
2026-08-11 15:15:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-function enviarW(e){e.preventDefault();const s=document.getElementById('serv').value;const c=document.getElementById('cant').value;const n=document.getElementById('nom').value;window.open(`https://wa.me/529997646962?text=${encodeURIComponent(`Hola EMK soy ${n} quiero ${s} cant ${c}`)}`,'_blank')}
+function enviarW(e){
+ e.preventDefault();
+ const s=document.getElementById('serv').value;
+ const c=document.getElementById('cant').value;
+ const n=document.getElementById('nom').value;
+ const msg=`Hola EMK Kukulcan, soy ${n}. Quiero cotizar: ${s} - Cantidad: ${c} pzas. Vengo de la web`;
+ window.open(`https://wa.me/529997646962?text=${encodeURIComponent(msg)}`,'_blank');
+}
